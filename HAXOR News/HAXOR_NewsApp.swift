b280192d64a2b441d98710be07e3f3628927c6e1
Chukwuebuka Ezepue <cezepue@getcarbon.co>
2025-10-02 13:26:47 +0100

    Initial Commit

---
+++ HAXOR News/HAXOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAXOR_NewsApp.swift
+//  HAXOR News
+//
+//  Created by Chukwuebuka Ezepue on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HAXOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
